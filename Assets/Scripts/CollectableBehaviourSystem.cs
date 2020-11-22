557254a6fc46107065b516d7898d0621910dd678
ThisIsKaluwa <aseriousmail@aol.com>
2020-11-22 11:56:42 +0100

    Removed math.sin from components, values only go from -1 to 1

--- Assets/Scripts/CollectableBehaviourSystem.cs
+++ Assets/Scripts/CollectableBehaviourSystem.cs
@@ -7,13 +7,17 @@ public class CollectableBehaviourSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        double time = math.sin(Time.ElapsedTime);
-        
+        double time = Time.ElapsedTime;
+
 
         Entities.ForEach((ref CollectedComponent collectedComp, ref Rotation rot) =>
         {
-            rot.Value = Quaternion.Euler(0, (float) (50*time) , 0);
-            
+            rot.Value = Quaternion.Euler(
+            0,
+            (float)(50 * time), //stays between values of -1 to 1
+            0
+            );
+
         }).Schedule();
     }
 }
